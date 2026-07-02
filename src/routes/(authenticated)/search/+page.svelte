675be8c8dd34a5a2a9c5bde6980556181888e86c
Simon Wiles <simonjwiles@gmail.com>
2026-07-01 21:34:04 -0700

    Format results

--- src/routes/(authenticated)/search/+page.svelte
+++ src/routes/(authenticated)/search/+page.svelte
@@ -5,6 +5,7 @@
 	import { InlineNotification } from 'carbon-components-svelte';
 
 	import { pb } from '$lib/pocketbase';
+	import { tooltip } from '$/lib/actions/tooltip';
 
 	let form = $state(/** @type {HTMLFormElement | null} */);
 	let results = $state();
@@ -107,11 +108,35 @@
 {#if results?.items.length}
 	<section id="results">
 		{#each results.items as result, i (/** @type {string} */ result.id)}
-			<!-- <pre>{JSON.stringify(result, null, 2)}</pre> -->
-			<div>
-				<a href={resolve('/image?imageId=' + result.id)}>{result.title}</a>
-				<pre>{JSON.stringify(searchObject(result, form.q.value), null, 2)}</pre>
-			</div>
+			{@const matches = searchObject(result.data, form.q.value)}
+			<section class="result">
+				<div class="match-details">
+					<h3><a href={resolve('/image?imageId=' + result.id)}>{result.title}</a></h3>
+					{#each matches as match (match.path)}
+						{@const path = match.path.split('.').slice(0, -1).join(' > ')}
+						{@const status = ''}
+						<span class="path">{path}</span>
+						<div class="field">
+							<span class="label">{match.key}:</span>
+							<span
+								class="value"
+								class:modified={status === 'modified'}
+								class:removed={status === 'removed'}
+								class:added={status === 'added'}
+								use:tooltip={{
+									content: `Original value: '[empty]'`,
+									placement: 'top-start',
+									enabled: status === 'modified'
+								}}
+								>{match.value}
+							</span>
+						</div>
+					{/each}
+				</div>
+				<div class="image">
+					<img src={result.image_url} alt={result.title} />
+				</div>
+			</section>
 		{/each}
 	</section>
 {:else}
@@ -119,7 +144,111 @@
 {/if}
 
 <style>
-	section {
+	input[type='search'] {
+		appearance: none;
+		background-clip: padding-box;
+		background-color: #fff;
+		border: 1px solid #ced4da;
+		border-radius: 0.375rem;
+		color: var(--text);
+		padding: 0.375rem 0.75rem;
+		transition:
+			border-color 0.15s ease-in-out,
+			box-shadow 0.15s ease-in-out;
+	}
+
+	div.search-types {
+		margin-top: 0.5rem;
+		margin-left: 1rem;
+	}
+
+	section#results {
 		overflow: auto;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		margin: 1rem 0;
+
+		.result {
+			background-color: white;
+			border-radius: var(--border-radius);
+			border: 2px solid var(--primary);
+			display: flex;
+			gap: 1rem;
+			margin: 0 auto;
+			overflow-wrap: anywhere;
+			padding: 1rem;
+			width: 100%;
+
+			> * {
+				width: 50%;
+				overflow: hidden;
+			}
+
+			.image {
+				position: relative;
+				overflow: scroll;
+
+				> img {
+					position: absolute;
+					width: 100%;
+				}
+			}
+		}
+
+		h3 {
+			padding-left: 2rem;
+			text-indent: -2rem;
+			font-size: 1.5rem;
+			margin-bottom: 0.5rem;
+		}
+	}
+
+	span.path {
+		display: block;
+		margin-top: 0.75rem;
+	}
+
+	div.field {
+		align-items: stretch;
+		display: flex;
+		flex-wrap: wrap;
+		font-size: 1rem;
+		gap: 0.5rem 0.25rem;
+		margin: 0.15rem 0;
+		padding: 0.25rem 0;
+	}
+
+	span.label {
+		display: inline-block;
+		min-width: 1.5rem;
+		text-align: right;
+		line-height: 1.25;
+	}
+
+	span.value {
+		background-color: #eaeaea;
+		flex: 1 1 0px;
+		line-height: 1.25;
+		margin: -0.25rem 0 -0.25rem 0.5rem;
+		min-width: 50%;
+		padding: 0.25rem;
+
+		&.modified {
+			background-color: hsl(from var(--primary) h s 85%);
+			outline: 2px dotted var(--primary);
+		}
+
+		&.added {
+			background-color: hsl(from var(--added) h s 85%);
+			outline: 2px dotted var(--added);
+		}
+
+		&.removed {
+			background-color: hsl(from var(--removed) h s 85%);
+			opacity: 0.6;
+			outline: 2px dotted var(--removed);
+			text-decoration: line-through;
+		}
 	}
 </style>
